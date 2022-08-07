b5d51c59e2f16ddad5b92adacb12c364ecf1c75d
Squiddleton <mdlegomaster@gmail.com>
2022-08-07 12:34:33 -0400

    Add TSDoc annotations

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import fetch from 'node-fetch';
 import Endpoints from './endpoints.js';
 import type { AES, AllCosmeticsOptions, AllNews, AnyData, AnyEndpointOptions, Banner, BannerColor, BaseStatOptions, ClientOptions, CombinedShop, Cosmetic, CosmeticSearchOptions, CreatorCode, IdStatsOptions, Language, Map, NameStatsOptions, NewCosmetics, News, NewsOptions, Playlist, PlaylistOptions, Shop, ShopOptions, Stats, StringRecord, Raw, RawFortniteAPIError } from './types.js';
+
 export * from './types.js';
 export { default as Endpoints } from './endpoints.js';
 
+/**
+ * An error thrown when Fortnite-API responds with a non-200 status
+ */
 export class FortniteAPIError extends Error {
+	/**
+	 * The error's status code
+	 */
 	code: number;
+	/**
+	 * The URL which threw the error
+	 */
 	route: string;
 	constructor(error: RawFortniteAPIError, route: string) {
 		super(error.error);
@@ -15,8 +25,17 @@ export class FortniteAPIError extends Error {
 	}
 }
 
+/**
+ * A class whose methods fetch data from Fortnite-API
+ */
 export class Client {
+	/**
+	 * The API key to use when calling Client#stats()
+	 */
 	key: string | null;
+	/**
+	 * The default language to use for all applicable methods
+	 */
 	language: Language;
 	constructor(options: ClientOptions = {}) {
 		this.key = options.key ?? null;
@@ -37,22 +56,57 @@ export class Client {
 		return res.data;
 	}
 
+	/**
+	 * Fetches the current AES key.
+	 *
+	 * @param keyFormat - The AES key's format
+	 * @returns Information about the current AES key
+	 */
 	async aes(keyFormat: 'hex' | 'base64' = 'hex') {
 		return this.fetch<AES>(this.route(Endpoints.AES, { keyFormat }));
 	}
 
+	/**
+	 * Fetches all banners.
+	 *
+	 * @param language - The language for the returned data
+	 * @returns An array of every banner
+	 */
 	async banners(language: Language = this.language) {
 		return this.fetch<Banner[]>(this.route(Endpoints.Banners, { language }));
 	}
+	/**
+	 * Fetches all banner colors.
+	 *
+	 * @returns An array of every banner color
+	 */
 	async bannerColors() {
 		return this.fetch<BannerColor[]>(this.route(Endpoints.BannerColors));
 	}
 
+	/**
+	 * Fetches information about a creator code.
+	 *
+	 * @param name - The creator code's name
+	 * @returns Information about the creator code
+	 */
 	async creatorCode(name: string) {
 		return this.fetch<CreatorCode>(this.route(Endpoints.CreatorCode, { name }));
 	}
 
-	listCosmetics(options: AllCosmeticsOptions & { combined: true }): Promise<NewCosmetics>;
+	/**
+	 * Lists recently-released cosmetics.
+	 *
+	 * @param options - Options for listing cosmetics
+	 * @returns An array of recently-released cosmetics
+	 */
+	listCosmetics(options: AllCosmeticsOptions & { new: true }): Promise<NewCosmetics>;
+	/**
+	 * Lists all cosmetics.
+	 *
+	 * @param options - Options for listing cosmetics
+	 * @returns An array of all cosmetics
+	 */
 	listCosmetics(options?: AllCosmeticsOptions): Promise<Cosmetic[]>;
 	async listCosmetics(options: AllCosmeticsOptions = {}) {
 		const params = { language: options.language ?? this.language };
@@ -60,6 +114,12 @@ export class Client {
 			? this.fetch<NewCosmetics>(this.route(Endpoints.NewCosmetics, params))
 			: this.fetch<Cosmetic[]>(this.route(Endpoints.CosmeticsList, params));
 	}
+	/**
+	 * Finds the first cosmetic that matches provided search parameters.
+	 *
+	 * @param options - Options for finding a cosmetic
+	 * @returns A cosmetic that matches the search parameters
+	 */
 	async findCosmetic(options: CosmeticSearchOptions<'single'>) {
 		const language = options.language ?? this.language;
 		return this.fetch<Cosmetic>(
@@ -68,6 +128,12 @@ export class Client {
 				: this.route(Endpoints.CosmeticsById.replace('{cosmetic-id}', options.id), { language })
 		);
 	}
+	/**
+	 * Finds all cosmetics that match provided search parameters.
+	 *
+	 * @param options - Options for filtering cosmetics
+	 * @returns All cosmetics that match the search parameters
+	 */
 	async filterCosmetics(options: CosmeticSearchOptions<'multiple'>) {
 		const language = options.language ?? this.language;
 		return this.fetch<Cosmetic[]>(
@@ -77,11 +143,29 @@ export class Client {
 		);
 	}
 
+	/**
+	 * Fetches information about the current Battle Royale map.
+	 *
+	 * @param language - The language for the returned data
+	 * @returns Information about the current map
+	 */
 	async map(language: Language = this.language) {
 		return this.fetch<Map>(this.route(Endpoints.Map, { language }));
 	}
 
+	/**
+	 * Fetches a specific mode's news.
+	 *
+	 * @param options - Options for fetching the news
+	 * @returns The provided mode's news
+	 */
 	news(options: NewsOptions & { mode: 'br' | 'stw' | 'creative' }): Promise<News>;
+	/**
+	 * Fetches every mode's news.
+	 *
+	 * @param options - Options for fetching the news
+	 * @returns All modes' news
+	 */
 	news(options?: NewsOptions): Promise<AllNews>;
 	async news(options: NewsOptions = {}) {
 		const params = { language: options.language ?? this.language };
@@ -94,7 +178,19 @@ export class Client {
 			}[options.mode], params));
 	}
 
+	/**
+	 * Fetches a playlist by its id.
+	 *
+	 * @param options - Options for fetching a playlist
+	 * @returns The playlist with the provided id
+	 */
 	playlists(options: PlaylistOptions & { id: string }): Promise<Playlist>;
+	/**
+	 * Fetches all playlists.
+	 *
+	 * @param options - Options for fetching all playlists
+	 * @returns An array of all playlists
+	 */
 	playlists(options?: PlaylistOptions): Promise<Playlist[]>;
 	async playlists(options: PlaylistOptions = {}) {
 		const params = { language: options.language ?? this.language };
@@ -103,7 +199,19 @@ export class Client {
 			: this.fetch<Playlist>(this.route(Endpoints.PlaylistById.replace('{playlist-id}', options.id), params));
 	}
 
+	/**
+	 * Fetches the current Battle Royale item shop with combined normal and special categories.
+	 *
+	 * @param options - Options for fetching the item shop
+	 * @returns The item shop with combined normal and special categories
+	 */
 	shop(options: ShopOptions & { combined: true }): Promise<CombinedShop>;
+	/**
+	 * Fetches the current Battle Royale item shop with separate normal and special categories.
+	 *
+	 * @param options - Options for fetching the item shop
+	 * @returns The item shop with separate normal and special categories
+	 */
 	shop(options?: ShopOptions): Promise<Shop>;
 	async shop(options: ShopOptions = {}) {
 		const params = { language: options.language ?? this.language };
@@ -112,6 +220,12 @@ export class Client {
 			: this.fetch<Shop>(this.route(options.combined ? Endpoints.BRShopCombined : Endpoints.BRShop, params));
 	}
 
+	/**
+	 * Fetches a user's stats by name or id.
+	 *
+	 * @param options - Options for fetching stats
+	 * @returns The user's stats
+	 */
 	async stats(options: NameStatsOptions | IdStatsOptions) {
 		if (this.key === null) throw new TypeError('Client#stats() requires an authorization key passed into the Client constructor options. You may request one at https://dash.fortnite-api.com/account');
 
--- src/types.ts
+++ src/types.ts
@@ -1,21 +1,52 @@
+/**
+ * A string that can be converted into a Date
+ */
 export type DateString = string;
 
+/**
+ * A unix timestamp in milliseconds
+ */
 export type UnixTimestamp = number;
 
+/**
+ * An object with string keys and string values
+ */
 export type StringRecord = {
 	[key: string]: string;
 };
 
+/**
+ * All supported languages for many endpoints
+ */
 export type Language = 'ar' | 'de' | 'en' | 'es' | 'es-419' | 'fr' | 'it' | 'ja' | 'ko' | 'pl' | 'pt-BR' | 'ru' | 'tr' | 'zh-CN' | 'zh-Hant';
 
+/**
+ * Options that support a language parameter
+ */
 export interface LanguageSupportingOption {
+	/**
+	 * The language for the returned data
+	 */
 	language?: Language;
 }
 
+/**
+ * Options for constructing a Client instance
+ */
 export interface ClientOptions extends LanguageSupportingOption {
+	/**
+	 * An API key used for Client#stats()
+	 *
+	 * @remarks
+	 *
+	 * A key can be obtained at {@link https://dash.fortnite-api.com/account}.
+	 */
 	key?: string;
 }
 
+/**
+ * A non-200 response received from Fortnite-API
+ */
 export interface RawFortniteAPIError {
 	status: 400 | 403 | 404;
 	error: string;
@@ -109,64 +140,165 @@ export interface NewCosmetics {
 	items: Cosmetic[];
 }
 
+/**
+ * Options for listing all cosmetics
+ */
 export interface AllCosmeticsOptions extends LanguageSupportingOption {
+	/**
+	 * Whether to only return new cosmetics
+	 */
 	new?: boolean;
 }
 
+/**
+ * Whether to search for single cosmetic or multiple cosmetics
+ */
 export type CosmeticSearchParametersType = 'single' | 'multiple';
 
+/**
+ * Options for finding or filtering cosmetics
+ */
 export interface CosmeticSearchOptions<T extends CosmeticSearchParametersType> extends LanguageSupportingOption {
+	/**
+	 * The search parameters' language
+	 */
 	searchLanguage?: string;
+	/**
+	 * The logic for comparing search parameters to the actual cosmetics
+	 *
+	 * @remarks
+	 *
+	 * Defaults to "full".
+	 */
 	matchMethod?: 'full' | 'contains' | 'starts' | 'ends';
+	/**
+	 * The cosmetic's id for Client#findCosmetics() or an array of ids for Client#filterCosmetics()
+	 */
 	id?: T extends 'single' ? string : string[];
+	/**
+	 * The cosmetic's in-game name
+	 */
 	name?: string;
+	/**
+	 * The cosmetic's description
+	 */
 	description?: string;
+	/**
+	 * The cosmetic's type
+	 */
 	type?: string;
+	/**
+	 * The cosmetic's in-game type
+	 */
 	displayType?: string;
+	/**
+	 * The cosmetic's internal type
+	 */
 	backendType?: string;
+	/**
+	 * The cosmetic's rarity
+	 */
 	rarity?: string;
+	/**
+	 * The cosmetic's in-game rarity
+	 */
 	displayRarity?: string;
+	/**
+	 * The cosmetic's internal rarity
+	 */
 	backendRarity?: string;
+	/**
+	 * Whether the cosmetic's rarity is part of a special series
+	 */
 	hasSeries?: boolean;
+	/**
+	 * The cosmetic's rarity series
+	 */
 	series?: string;
+	/**
+	 * The cosmetic's internal rarity series
+	 */
 	backendSeries?: string;
+	/**
+	 * Whether the cosmetic is part of a set
+	 */
 	hasSet?: boolean;
+	/**
+	 * The cosmetic's set
+	 */
 	set?: string;
+	/**
+	 * The cosmetic's set text
+	 */
 	setText?: string;
+	/**
+	 * The cosmetic's internal set
+	 */
 	backendSet?: string;
+	/**
+	 * Whether the cosmetic has an introduction
+	 */
 	hasIntroduction?: boolean;
+	/**
+	 * The cosmetic's internal introduction
+	 */
 	backendIntroduction?: number;
+	/**
+	 * The chapter that the cosmetic was introduced in
+	 */
 	introductionChapter?: string;
+	/**
+	 * The season that the cosmetic was introduced in
+	 */
 	introductionSeason?: string;
+	/**
+	 * Whether the cosmetic has a featured image
+	 */
 	hasFeaturedImage?: boolean;
+	/**
+	 * Whether the cosmetic has variants
+	 */
 	hasVariants?: boolean;
+	/**
+	 * Whether the cosmetic has gameplay tags
+	 */
 	hasGameplayTags?: boolean;
+	/**
+	 * One of the cosmetic's gameplay tags
+	 */
 	gameplayTag?: string;
+	/**
+	 * Whether the cosmetic has meta tags
+	 */
 	hasMetaTags?: boolean;
+	/**
+	 * One of the cosmetic's meta tags
+	 */
 	metaTag?: string;
+	/**
+	 * Whether the cosmetic has a dynamic pak id
+	 */
 	hasDynamicPakId?: boolean;
+	/**
+	 * The cosmetic's dynamic pak id
+	 */
 	dynamicPakId?: string;
+	/**
+	 * The timestamp when the cosmetic was added to the game
+	 */
 	added?: UnixTimestamp;
+	/**
+	 * The time since the cosmetic was added to the game
+	 */
 	addedSince?: UnixTimestamp;
+	/**
+	 * The time since the cosmetic last appeared
+	 */
 	unseenFor?: number;
+	/**
+	 * The timestamp of the cosmetic's last appearance
+	 */
 	lastAppearance?: UnixTimestamp;
-}
-
-export interface BaseBRStats {
-	score: number;
-	scorePerMin: number;
-	scorePerMatch: number;
-	wins: number;
-	kills: number;
-	killsPerMin: number;
-	killsPerMatch: number;
-	deaths: number;
-	kd: number;
-	matches: number;
-	winRate: number;
-	minutesPlayed: number;
-	playersOutlived: number;
-	lastModified: DateString;
 }
 
 export interface CreatorCode {
@@ -224,6 +356,13 @@ export interface AllNews {
 }
 
 export interface NewsOptions extends LanguageSupportingOption {
+	/**
+	 * The mode whose news will be fetched
+	 *
+	 * @remarks
+	 *
+	 * Defaults to return all modes' news.
+	 */
 	mode?: 'br' | 'stw' | 'creative';
 }
 
@@ -255,6 +394,9 @@ export interface Playlist {
 }
 
 export interface PlaylistOptions extends LanguageSupportingOption {
+	/**
+	 * The playlist's id
+	 */
 	id?: string;
 }
 
@@ -381,20 +523,40 @@ export interface Shop extends CombinedShop {
 }
 
 export interface ShopOptions extends LanguageSupportingOption {
+	/**
+	 * Whether to combine the normal and special shop categories
+	 */
 	combined?: boolean;
 }
 
-export interface SoloStats extends BaseBRStats {
+export interface BaseStats {
+	score: number;
+	scorePerMin: number;
+	scorePerMatch: number;
+	wins: number;
+	kills: number;
+	killsPerMin: number;
+	killsPerMatch: number;
+	deaths: number;
+	kd: number;
+	matches: number;
+	winRate: number;
+	minutesPlayed: number;
+	playersOutlived: number;
+	lastModified: DateString;
+}
+
+export interface SoloStats extends BaseStats {
 	top10: number;
 	top25: number;
 }
 
-export interface DuoStats extends BaseBRStats {
+export interface DuoStats extends BaseStats {
 	top5: number;
 	top12: number;
 }
 
-export interface SquadStats extends BaseBRStats {
+export interface SquadStats extends BaseStats {
 	top3: number;
 	top6: number;
 }
@@ -407,7 +569,7 @@ export interface PlatformStats {
 	duo: DuoStats;
 	trio: null;
 	squad: SquadStats;
-	ltm: BaseBRStats;
+	ltm: BaseStats;
 }
 
 export interface Stats {
@@ -429,24 +591,60 @@ export interface Stats {
 }
 
 export interface BaseStatOptions {
+	/**
+	 * The time period of stats to display
+	 *
+	 * @remarks
+	 *
+	 * Defaults to "lifetime".
+	 */
 	timeWindow?: 'season' | 'lifetime';
+	/**
+	 * Whether to include an automatically-generated image of a control input's stats
+	 *
+	 * @remarks
+	 *
+	 * Stats#image returns null by default.
+	 */
 	image?: 'all' | 'keyboardMouse' | 'gamepad' | 'touch';
 }
 
 export interface NameStatsOptions extends BaseStatOptions {
+	/**
+	 * The account's name
+	 */
 	name: string;
+	/**
+	 * The account's platform
+	 *
+	 * @remarks
+	 *
+	 * Defaults to "epic".
+	 */
 	accountType?: 'epic' | 'psn' | 'xbl';
 }
 
 export interface IdStatsOptions extends BaseStatOptions {
+	/**
+	 * The account's id
+	 */
 	id: string;
 }
 
+/**
+ * Any type of data that Fortnite-API can return from a 200 response
+ */
 export type AnyData = AES | Banner[] | BannerColor[] | NewCosmetics | Cosmetic | Cosmetic[] | CreatorCode | Map | News | AllNews | Playlist | Playlist[] | CombinedShop | Shop | Stats;
 
+/**
+ * The data that Fortnite-API directly returns from a 200 response
+ */
 export interface Raw<Data extends AnyData> {
 	status: 200;
 	data: Data;
 }
 
+/**
+ * Any options object used as a Client method's parameter
+ */
 export type AnyEndpointOptions = AllCosmeticsOptions | CosmeticSearchOptions<CosmeticSearchParametersType> | NewsOptions | PlaylistOptions | ShopOptions | BaseStatOptions | NameStatsOptions | IdStatsOptions;
